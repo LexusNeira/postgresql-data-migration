29d91e91265ac1fa5c20d17f340ed21bb2a75aab
Mert Yavaşca <mert_yavasca@hotmail.com>
2023-05-25 23:29:52 +0300

    Create datamigration.sql

---
+++ datamigration.sql
@@ -0,0 +1,45 @@
+CREATE OR REPLACE PROCEDURE transfer_data(chunkSize INT DEFAULT 10 ) 
+--    Postgresql data migration between two table in same database
+--    
+--    author: Mert Yavasca
+--    created_date: 24.05.2023
+--    last_modify_date: 24.05.2023 / kesilirse kaldıgı yerden devam etmesi icin kontrol eklendi.
+--    
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    totalRows INT;
+    iterationCount INT;
+    destMaxId INT;    
+    counter INT := 0;
+
+BEGIN
+                
+    SELECT max(id) INTO destMaxId FROM destination_table;  --kesilirse nerden devam edecegini anlaması icin hedef tabloya bakiyor.
+    
+    if destMaxId is null then
+        destMaxId := 0;
+    end if;
+    
+
+    SELECT COUNT(*) INTO totalRows FROM b;
+
+    iterationCount := CEIL((totalRows-destMaxId)::NUMERIC / chunkSize);
+                               
+    FOR i IN 0..iterationCount-1 LOOP
+        begin
+                        INSERT INTO destination_table (id) 
+                        SELECT id 
+                        FROM source_table
+                        ORDER BY id 
+                        OFFSET ((i * chunkSize) + destMaxId) 
+                        LIMIT chunkSize;
+                        counter := counter + 1;
+                        commit;                                              
+        end;                                                              
+    END LOOP;         
+
+    raise notice '% kez iterasyon yapildi, % satir tasindi',counter,chunkSize*counter;
+
+END;
+$$;
